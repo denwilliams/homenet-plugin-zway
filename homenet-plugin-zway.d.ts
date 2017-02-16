61742188f4de45a738f20880d2b8af72fb0de747
Den Williams <work@denwilliams.net>
2017-02-16 22:40:48 +1100

    Package name

--- homenet-plugin-zway.d.ts
+++ homenet-plugin-zway.d.ts
@@ -1,4 +1,4 @@
 declare module 'homenet-plugin-zway' {
-  import { IPluginLoader } from 'homenet-core';
+  import { IPluginLoader } from '@homenet/core';
   export var ZwayPluginLoader: new (...args: any[]) => IPluginLoader
 }
